05a81a4e76116921c018590ebf8a39e88eb2b2ee
ericbsd <ericturgeon.bsd@gmail.com>
2019-08-02 15:53:47 -0300

    adding rest.py to use REST API for some UI test

---
+++ tests/rest.py
@@ -0,0 +1,47 @@
+import requests
+import json
+from config import ip
+from source import user, password
+
+header = {'Content-Type': 'application/json', 'Vary': 'accept'}
+authentification = (user, password)
+api_url = f"{ip}/api/v2.0"
+
+
+def get(testpath):
+    getit = requests.get(
+        api_url + testpath,
+        headers=header,
+        auth=authentification
+    )
+    return getit
+
+
+def post(testpath, payload=None):
+    postit = requests.post(
+        api_url + testpath,
+        headers=header,
+        auth=authentification,
+        data=json.dumps(payload)
+    )
+    return postit
+
+
+def put(testpath, payload):
+    putit = requests.put(
+        api_url + testpath,
+        headers=header,
+        auth=authentification,
+        data=json.dumps(payload)
+    )
+    return putit
+
+
+def delete(testpath, payload=None):
+    deleteit = requests.delete(
+        api_url + testpath,
+        headers=header,
+        auth=authentification,
+        data=json.dumps(payload) if payload else None
+    )
+    return deleteit
