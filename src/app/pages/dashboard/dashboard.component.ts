7e07f200a24a31b0dddc621caa4c8a5796d8d28c
Damian Szidiropulosz <damianos@knoxbug.org>
2020-06-09 15:23:08 -0400

    Handle situations where API returns the wrong type

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -171,7 +171,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
         this.isFooterConsoleOpen = res.consolemsg;
       }
     });
-    console.log(this.isFN);
+    
     if(this.isFN !== null && this.isFN.toString() == 'false'){
       this.ws.call('failover.licensed').subscribe((res)=> {
         if (res) {
